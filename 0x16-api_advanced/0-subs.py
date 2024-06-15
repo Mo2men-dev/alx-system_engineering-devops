82fb0560747c4bfa412557a7111fdfdf610cf949
Momen Sameh <momensameh5@gmail.com>
2024-06-15 10:47:52 -0700

    update to first script

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+""" How many subs """
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+    """
+        that queries the Reddit API and returns the number of subscribers
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    user_agent = {'User-agent': 'Google Chrome Version 118.0.5993.120'}
+    response = requests.get(url, headers=user_agent, allow_redirects=False)
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        return 0
-
-    return sub_info.json().get("data").get("subscribers")
+    if response.status_code == 200:
+        data = response.json()
+        subs = data['data']['subscribers']
+        return (subs)
+    else:
+        return (0)
